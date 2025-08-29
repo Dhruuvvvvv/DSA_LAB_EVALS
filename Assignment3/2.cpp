f55cc27298720cdb5eb24a0e3c247c04a1e93fc4
Dhruv Gupta <dgupta8_be24@thapar.edu>
2025-08-29 15:15:09 +0530

    Update 2.cpp

--- Assignment3/2.cpp
+++ Assignment3/2.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
 using namespace std;
-
 #define MAX 100   
-
 class Stack {
     char arr[MAX];
     int top;
-
 public:
     Stack() { top = -1; }
-
     bool isFull() {
         return (top == MAX - 1);
     }
-
     bool isEmpty() {
         return (top == -1);
     }
-
     void push(char x) {
         if (isFull()) {
             cout << "Stack Overflow!\n";
@@ -25,7 +19,6 @@ public:
         }
         arr[++top] = x;
     }
-
     char pop() {
         if (isEmpty()) {
             cout << "Stack Underflow!\n";
@@ -34,25 +27,18 @@ public:
         return arr[top--];
     }
 };
-
 int main() {
     Stack s;
     string str;
-
     cout << "Enter a string: ";
     cin >> str;  
-
-
     for (int i = 0; i < str.length(); i++) {
         s.push(str[i]);
-    }
-
-    
+    } 
     cout << "Reversed string: ";
     while (!s.isEmpty()) {
         cout << s.pop();
     }
     cout << endl;
-
     return 0;
 }
